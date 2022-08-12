047a19d0500a6ae1c7ebf450f923feabc8bad636
Nanang Koesharwanto <nanangkoesharwanto@gmail.com>
2022-08-13 01:10:31 +0700

    profile_page

--- lib/views/main/profile/profile_page.dart
+++ lib/views/main/profile/profile_page.dart
@@ -27,185 +27,190 @@ class _ProfilePageState extends State<ProfilePage> {
           ),
         ],
       ),
-      body: Column(
-        children: [
-          Container(
-            padding: const EdgeInsets.only(
-              top: 28,
-              bottom: 40,
-              left: 15,
-              right: 15,
-            ),
-            decoration: BoxDecoration(
-              color: R.colors.primary,
-              borderRadius: const BorderRadius.only(
-                bottomLeft: Radius.circular(9),
-                bottomRight: Radius.circular(9),
+      body: SafeArea(
+        child: ListView(
+          children: [
+            Container(
+              padding: const EdgeInsets.only(
+                top: 28,
+                bottom: 60,
+                left: 15,
+                right: 15,
+              ),
+              decoration: BoxDecoration(
+                color: R.colors.primary,
+                borderRadius: const BorderRadius.only(
+                  bottomLeft: Radius.circular(9),
+                  bottomRight: Radius.circular(9),
+                ),
+              ),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: const [
+                        Text(
+                          "Nama User",
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w400,
+                            color: Colors.white,
+                          ),
+                        ),
+                        Text(
+                          "Nama Sekolah",
+                          style: TextStyle(
+                            fontSize: 12,
+                            fontWeight: FontWeight.w400,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Image.asset(
+                    R.assets.imgUser,
+                    height: 50,
+                    width: 50,
+                  )
+                ],
               ),
             ),
-            child: Row(
-              children: [
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: const [
-                      Text(
-                        "Nama User",
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.w400,
-                          color: Colors.white,
-                        ),
-                      ),
-                      Text(
-                        "Nama Sekolah",
-                        style: TextStyle(
-                          fontSize: 12,
-                          fontWeight: FontWeight.w400,
-                          color: Colors.white,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                Image.asset(
-                  R.assets.imgUser,
-                  height: 50,
-                  width: 50,
-                )
-              ],
-            ),
-          ),
-          Container(
-            width: double.infinity,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(10),
-              boxShadow: [
-                BoxShadow(
-                  blurRadius: 7,
-                  color: Colors.black.withOpacity(0.25),
-                )
-              ],
-            ),
-            margin: const EdgeInsets.symmetric(
-              vertical: 18,
-              horizontal: 13,
-            ),
-            padding: const EdgeInsets.symmetric(
-              vertical: 18,
-              horizontal: 13,
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text("Identitas Diri"),
-                const SizedBox(height: 15),
-                Text(
-                  "Nama Lengkap",
-                  style: TextStyle(
-                    color: R.colors.greSubtitleHome,
-                    fontSize: 12,
-                  ),
-                ),
-                const Text(
-                  "Nama Lengkap User",
-                  style: TextStyle(
-                    fontSize: 13,
-                  ),
-                ),
-                const SizedBox(height: 15),
-                Text(
-                  "Nama Lengkap",
-                  style: TextStyle(
-                    color: R.colors.greSubtitleHome,
-                    fontSize: 12,
-                  ),
-                ),
-                const Text(
-                  "Nama Lengkap User",
-                  style: TextStyle(
-                    fontSize: 13,
-                  ),
-                ),
-                const SizedBox(height: 15),
-                Text(
-                  "Nama Lengkap",
-                  style: TextStyle(
-                    color: R.colors.greSubtitleHome,
-                    fontSize: 12,
-                  ),
-                ),
-                const Text(
-                  "Nama Lengkap User",
-                  style: TextStyle(
-                    fontSize: 13,
-                  ),
-                ),
-                const SizedBox(height: 15),
-                Text(
-                  "Nama Lengkap",
-                  style: TextStyle(
-                    color: R.colors.greSubtitleHome,
-                    fontSize: 12,
-                  ),
-                ),
-                const Text(
-                  "Nama Lengkap User",
-                  style: TextStyle(
-                    fontSize: 13,
-                  ),
-                ),
-                const SizedBox(height: 15),
-                Text(
-                  "Nama Lengkap",
-                  style: TextStyle(
-                    color: R.colors.greSubtitleHome,
-                    fontSize: 12,
-                  ),
-                ),
-                const Text(
-                  "Nama Lengkap User",
-                  style: TextStyle(
-                    fontSize: 13,
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Container(
-            margin: const EdgeInsets.symmetric(
-              horizontal: 13,
-            ),
-            padding: const EdgeInsets.symmetric(
-              horizontal: 16,
-              vertical: 8,
-            ),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(10),
-              boxShadow: [
-                BoxShadow(
-                  blurRadius: 7,
-                  color: Colors.black.withOpacity(0.25),
-                )
-              ],
-            ),
-            child: Row(children: const [
-              Icon(
-                Icons.exit_to_app,
-                color: Colors.red,
+            Container(
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: [
+                  BoxShadow(
+                    blurRadius: 7,
+                    color: Colors.black.withOpacity(0.25),
+                  )
+                ],
               ),
-              SizedBox(width: 10),
-              Text(
-                "Title",
-                style: TextStyle(
+              margin: const EdgeInsets.symmetric(
+                vertical: 18,
+                horizontal: 13,
+              ),
+              padding: const EdgeInsets.symmetric(
+                vertical: 18,
+                horizontal: 13,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text("Identitas Diri"),
+                  const SizedBox(height: 15),
+                  Text(
+                    "Nama Lengkap",
+                    style: TextStyle(
+                      color: R.colors.greSubtitleHome,
+                      fontSize: 12,
+                    ),
+                  ),
+                  const Text(
+                    "Nama Lengkap User",
+                    style: TextStyle(
+                      fontSize: 13,
+                    ),
+                  ),
+                  const SizedBox(height: 15),
+                  Text(
+                    "Nama Lengkap",
+                    style: TextStyle(
+                      color: R.colors.greSubtitleHome,
+                      fontSize: 12,
+                    ),
+                  ),
+                  const Text(
+                    "Nama Lengkap User",
+                    style: TextStyle(
+                      fontSize: 13,
+                    ),
+                  ),
+                  const SizedBox(height: 15),
+                  Text(
+                    "Nama Lengkap",
+                    style: TextStyle(
+                      color: R.colors.greSubtitleHome,
+                      fontSize: 12,
+                    ),
+                  ),
+                  const Text(
+                    "Nama Lengkap User",
+                    style: TextStyle(
+                      fontSize: 13,
+                    ),
+                  ),
+                  const SizedBox(height: 15),
+                  Text(
+                    "Nama Lengkap",
+                    style: TextStyle(
+                      color: R.colors.greSubtitleHome,
+                      fontSize: 12,
+                    ),
+                  ),
+                  const Text(
+                    "Nama Lengkap User",
+                    style: TextStyle(
+                      fontSize: 13,
+                    ),
+                  ),
+                  const SizedBox(height: 15),
+                  Text(
+                    "Nama Lengkap",
+                    style: TextStyle(
+                      color: R.colors.greSubtitleHome,
+                      fontSize: 12,
+                    ),
+                  ),
+                  const Text(
+                    "Nama Lengkap User",
+                    style: TextStyle(
+                      fontSize: 13,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(
+                horizontal: 13,
+              ),
+              padding: const EdgeInsets.symmetric(
+                horizontal: 16,
+                vertical: 8,
+              ),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: [
+                  BoxShadow(
+                    blurRadius: 7,
+                    color: Colors.black.withOpacity(0.25),
+                  )
+                ],
+              ),
+              child: Row(children: const [
+                Icon(
+                  Icons.exit_to_app,
                   color: Colors.red,
                 ),
-              ),
-            ]),
-          )
-        ],
+                SizedBox(width: 10),
+                Text(
+                  "Title",
+                  style: TextStyle(
+                    color: Colors.red,
+                  ),
+                ),
+              ]),
+            ),
+            const SizedBox(
+              height: 35,
+            )
+          ],
+        ),
       ),
     );
   }
